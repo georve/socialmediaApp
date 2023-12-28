4bc019ecaf1a5b28d3abcc142a498855b55a743e
Georman <georman.calderon@cleteci.com>
2023-12-27 20:35:49 -0400

    FlatList

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,56 +1,15 @@
 import FeedPost from "../components/FeedPost";
-import { ScrollView } from "react-native";
+import { ScrollView, FlatList } from "react-native";
+import posts from "../../assets/data/posts.json";
 
-const post1 = {
-    id: "p1",
-    createdAt: "19 m", 
-    User: {
-      id: "u1",
-      image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-      name: "Vadim Savin",
-    },
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-    numberOfLikes: 11,
-    numberOfShares: 2,
-  };
-  
-  const post2 = {
-    id: "p2",
-    createdAt: "10 m", 
-    User: {
-      id: "u2",
-      image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-      name: "Elon Musk",
-    },
-    description: "Today we launched another rocket 🚀",
-    numberOfLikes: 11,
-    numberOfShares: 2,
-  };
-  const post3 = {
-    id: "p2",
-    createdAt: "10 m", 
-    User: {
-      id: "u2",
-      image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
-      name: "Jeff",
-    },
-    description: "View from my office 😍",
-    image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
-    numberOfLikes: 11,
-    numberOfShares: 2,
-  };
+
   const FeedScreen = () => {
     return (
-      <ScrollView>
-        <FeedPost post={post1} />
-        <FeedPost post={post2} />
-        <FeedPost post={post3} />
-      </ScrollView>
+        <FlatList
+        data={posts}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <FeedPost post={item} />}
+      />
     );
   };
 
