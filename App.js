e08eea44dba33dba635aae6ba8e26db0c03b8632
Georman <georman.calderon@cleteci.com>
2023-12-28 20:19:43 -0400

    create posts

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from "react-native";
 import FeedScreen from './src/screens/FeedScreen';
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FeedScreen/>
+
       <StatusBar style="auto" />
+      <CreatePostScreen />
     </View>
   );
 }
